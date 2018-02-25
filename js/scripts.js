b58440e6ce16e4dffaf4420d0527be303e8a8e7c
jg <jasmine.booyakka@gmail.com>
2018-02-25 13:33:29 -0800

    changed div ids to div classes for destinations but lost div class=row

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
-$(document).ready(function() {
-  $("#adventure form").submit(function() {
-    $(".one").append("blah blah");
-    $(".two").append("blah blah");
-    $(".three").append("blah blah");
-    $(".four").append("blah blah");
-    $(".five").append("blah blah");
+ $(document).ready(function() {
+//   $("#adventure form").submit(function() {
+//     $(".one").append("blah blah");
+//     $(".two").append("blah blah");
+//     $(".three").append("blah blah");
+//     $(".four").append("blah blah");
+//     $(".five").append("blah blah");
 
-$("destinations").show();
+$("destinations").show(function() {
+  $(".one").select(function() {
+    $("TROPICAL").show();
+  });
+});
 
 <input name = "firstname" type="text">
 
-function validateForm() {
-  var name = document.forms["myForm"]["fname"].value;
-  if (name === "") {
-    alert ("Please enter a name!");
-    return false;
+// function validateForm() {
+//  var name = document.forms["myForm"]["fname"].value;
+//  if (name === "") {
+//    alert ("Please enter a name!");
+//    return false;
   });
 });
